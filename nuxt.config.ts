2d0be9ff0b0021f743637bd7ad20e1e1db0c778d
MarvinHongX <marvinhongx@gmail.com>
2024-08-29 15:22:11 +0900

    Feature Enhancement

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,14 +21,15 @@ export default defineNuxtConfig({
                 { name: "google-site-verification", content: "fRM5FTZ5bZt5MRpwplDneJwVFNFflKfq-jVZrpXB3kc" },
               ],
             script: [
-                { src: 'https://www.googletagmanager.com/gtag/js?id=UA-93461466-1' },
+                { src: 'https://www.googletagmanager.com/ns.html?id=GTM-MZNPBKW7' },
                 {
                     id: 'ga-analytics',
                     children: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', 'UA-93461466-1');
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-MZNPBKW7');
                     `
                 }
             ],
