85ebdb3f46c192c388610cec3a6408e765fa9f72
MarvinHongX <marvinhongx@gmail.com>
2025-08-12 11:33:42 +0900

    Feature Enhancement

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,29 +20,6 @@ export default defineNuxtConfig({
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
                 { hid: 'description', name: 'description', content: 'Your Premier Block Explorer and Analytics Platform for Bitcoin' },
-                { name: "google-site-verification", content: "fRM5FTZ5bZt5MRpwplDneJwVFNFflKfq-jVZrpXB3kc" },
-                { name: "google-adsense-account", content: "ca-pub-9944647203025348" },
-            ],
-            script: [
-                { 
-                    src: 'https://www.googletagmanager.com/ns.html?id=GTM-MZNPBKW7',
-                    async: true,
-                },
-                {
-                    id: 'ga-analytics',
-                    children: `
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-MZNPBKW7');
-                    `
-                },
-                {
-                    src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9944647203025348',
-                    async: true,
-                    crossorigin: 'anonymous',
-                },
             ],
             link: [
                 {
